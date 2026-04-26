c1be6eba15e40b06b442752143d4954cadfd97ea
Welinson <welinsonmarlon15@gmail.com>
2026-04-26 10:09:04 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   );
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+await app.listen(port, '0.0.0.0');
   console.log(`🚀 ArloFlux API está a rodar na porta: ${port}`);
 }
 
